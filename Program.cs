4273f645c8db43b5e18c406ec8fdae717bce0c99
majda107 <majda.trpkos@gmail.com>
2019-04-11 19:56:44 +0200

    Changed window title name

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace OpenGLCamera
     {
         static void Main(string[] args)
         {
-            GameWindow gw = new GameWindow(500, 500, new OpenTK.Graphics.GraphicsMode(32, 24, 0, 4), "OpenGL camera");
+            GameWindow gw = new GameWindow(500, 500, new OpenTK.Graphics.GraphicsMode(32, 24, 0, 4), "Sharp engine");
 
             Game game = new Game(gw, 60);
             game.Start();
