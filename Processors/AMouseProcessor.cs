6a0a37591e78948e4e53ebbb65cac2f6991a826c
majda107 <majda.trpkos@gmail.com>
2019-04-11 23:28:53 +0200

    Moved processors to it's own folder, created Mouse processor

---
+++ Processors/AMouseProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Input;
+
+namespace SharpEngine.Processors
+{
+    abstract class AMouseProcessor
+    {
+        public int x { get; protected set; }
+        public int y { get; protected set; }
+
+        public int xDev { get; protected set; }
+        public int yDev { get; protected set; }
+
+        public abstract void Update(MouseState currentState);
+    }
+}
---
+++ Processors/MouseProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Input;
+
+namespace SharpEngine.Processors
+{
+    class MouseProcessor : AMouseProcessor
+    {
+        private MouseState prevState;
+
+        public override void Update(MouseState currentState)
+        {
+            this.x = currentState.X;
+            this.y = currentState.Y;
+
+            this.xDev = currentState.X - this.prevState.X;
+            this.yDev = currentState.Y - this.prevState.Y;
+
+            this.prevState = currentState;
+        }
+    }
+}
