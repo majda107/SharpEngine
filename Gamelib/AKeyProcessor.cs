3762bb4b37d4d9b78c3f4af90b90d42c55708dd8
majda107 <majda.trpkos@gmail.com>
2019-04-11 19:40:19 +0200

    Created Key(board) processor

---
+++ Gamelib/AKeyProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Input;
+
+namespace OpenGLCamera.Gamelib
+{
+    abstract class AKeyProcessor
+    {
+        public List<Key> keysDown{ get; private set; }
+        public AKeyProcessor()
+        {
+            this.keysDown = new List<Key>();
+        }
+    }
+}
