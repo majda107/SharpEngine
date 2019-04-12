532e5c22ae4d7c42e62e8806000f8b5df4999aea
majda107 <majda.trpkos@gmail.com>
2019-04-13 00:44:13 +0200

    Added hitbox class

---
+++ Gamelib/Hitbox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace SharpEngine.Gamelib
+{
+    class Hitbox : AHitbox
+    {
+        public Hitbox(Vector3 first, Vector3 second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+    }
+}
