c970fa7dbbe64b22e2b398da67bc79afbafa3233
majda107 <majda.trpkos@gmail.com>
2019-04-13 01:05:07 +0200

    Render function completed

--- Gamelib/AHitbox.cs
+++ Gamelib/AHitbox.cs
@@ -13,6 +13,7 @@ namespace SharpEngine.Gamelib
         public bool visible { get; set; }
         public AHitbox()
         {
+            this.offset = 0.2f;
             this.visible = false;
         }
         /// <summary>
@@ -24,17 +25,48 @@ namespace SharpEngine.Gamelib
         /// </summary>
         public Vector3 second { get; protected set; }
 
+        public float offset { get; set; }
         public void Render()
         {
             GL.Color4(0.3, 0.3, 0.7, 0.4);
 
             GL.Begin(PrimitiveType.Quads);
 
-            // front
-            GL.Vertex3(first.X, second.Y, first.Z - 0.5);
-            GL.Vertex3(first.X, first.Y, first.Z - 0.5);
-            GL.Vertex3(second.X, first.Y, first.Z - 0.5);
-            GL.Vertex3(second.X, second.Y, first.Z - 0.5);
+            //front
+            GL.Vertex3(first.X, second.Y, first.Z - offset);
+            GL.Vertex3(first.X, first.Y, first.Z - offset);
+            GL.Vertex3(second.X, first.Y, first.Z - offset);
+            GL.Vertex3(second.X, second.Y, first.Z - offset);
+
+            //back
+            GL.Vertex3(first.X, second.Y, second.Z + offset);
+            GL.Vertex3(first.X, first.Y, second.Z + offset);
+            GL.Vertex3(second.X, first.Y, second.Z + offset);
+            GL.Vertex3(second.X, second.Y, second.Z + offset);
+
+            //left
+            GL.Vertex3(first.X + offset, second.Y, second.Z);
+            GL.Vertex3(first.X + offset, first.Y, second.Z);
+            GL.Vertex3(first.X + offset, first.Y, first.Z);
+            GL.Vertex3(first.X + offset, second.Y, first.Z);
+
+            //right
+            GL.Vertex3(second.X - offset, second.Y, second.Z);
+            GL.Vertex3(second.X - offset, first.Y, second.Z);
+            GL.Vertex3(second.X - offset, first.Y, first.Z);
+            GL.Vertex3(second.X - offset, second.Y, first.Z);
+
+            //top
+            GL.Vertex3(first.X, second.Y + offset, second.Z);
+            GL.Vertex3(first.X, second.Y + offset, first.Z);
+            GL.Vertex3(second.X, second.Y + offset, first.Z);
+            GL.Vertex3(second.X, second.Y + offset, second.Z);
+
+            //bottom
+            GL.Vertex3(first.X, first.Y - offset, second.Z);
+            GL.Vertex3(first.X, first.Y - offset, first.Z);
+            GL.Vertex3(second.X, first.Y - offset, first.Z);
+            GL.Vertex3(second.X, first.Y - offset, second.Z);
 
             GL.End();
         }
