8d4c0e5b53f673ade3c2fec8494c61a4cb6f61f5
majda107 <majda.trpkos@gmail.com>
2019-04-13 20:10:50 +0200

    Created Vertex buffer object

---
+++ ObjLoader/VertexBuffer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace SharpEngine.ObjLoader
+{
+    class VertexBuffer
+    {
+        public List<Vector3> vertices;
+        public List<Vector2> textures;
+        public List<Vector3> normals;
+        public VertexBuffer()
+        {
+            this.vertices = new List<Vector3>();
+            this.textures = new List<Vector2>();
+            this.normals = new List<Vector3>();
+        }
+    }
+}
