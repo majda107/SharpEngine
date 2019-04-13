441d2478491b6d4816a97ddcdd4ea07bceac7715
majda107 <majda.trpkos@gmail.com>
2019-04-13 02:45:05 +0200

    Implemented .obj loader

---
+++ ObjLoader/Face3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace SharpEngine.ObjLoader
+{
+    struct Face3
+    {
+        public Vector3[] vertices;
+        public Vector2[] textures;
+        public Vector3[] normals;
+        public Face3(Vector3[] vertices, Vector2[] textures, Vector3[] normals)
+        {
+            this.vertices = vertices;
+            this.textures = textures;
+            this.normals = normals;
+        }
+    }
+}
