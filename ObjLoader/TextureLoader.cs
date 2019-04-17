bba331d88ac1ddb19481b22814b75ab19498e72c
majda107 <majda.trpkos@gmail.com>
2019-04-17 16:58:23 +0200

    Added texture loader class

---
+++ ObjLoader/TextureLoader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.IO;
+
+namespace SharpEngine.ObjLoader
+{
+    class TextureLoader
+    {
+        public static int LoadBitmap(Bitmap textureMap)
+        {
+            int texID = GL.GenTexture();
+
+            GL.BindTexture(TextureTarget.Texture2D, texID);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)All.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)All.Linear);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)All.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)All.ClampToEdge);
+
+            BitmapData data = textureMap.LockBits(new Rectangle(0, 0, textureMap.Width, textureMap.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+            textureMap.UnlockBits(data);
+
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+
+            return texID;
+        }
+
+        public static int LoadFromPath(string path)
+        {
+            Bitmap bitmap = new Bitmap(path);
+            return LoadBitmap(bitmap);
+        }
+    }
+}
