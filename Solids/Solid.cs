05a445244c25441ce54ab8c1b8b8a53f54a70559
majda107 <majda.trpkos@gmail.com>
2019-04-19 15:30:45 +0200

    Hitbox update method overridden, removed unnecessary things

--- Solids/Solid.cs
+++ Solids/Solid.cs
@@ -12,14 +12,14 @@ namespace SharpEngine.Solids
 {
     class Solid : ASolid
     {
-        private Face3[] faces;
+        
         public Solid(Face3[] faces, Vector3 pos) : base(pos)
         {
             this.faces = faces;
-            this.hitbox = CreateHitbox(ref faces);
+            this.hitbox = UpdateHitbox(ref faces);
         }
 
-        public Hitbox CreateHitbox(ref Face3[] faces)
+        protected override Hitbox UpdateHitbox(ref Face3[] faces)
         {
             float left = faces[0].vertices[0].X;
             float right = faces[0].vertices[0].X;
@@ -41,18 +41,12 @@ namespace SharpEngine.Solids
                 }
             }
 
-            return new Hitbox(new Vector3(left, bottom, close) + pos, new Vector3(right, top, distant) + pos);
+            return new Hitbox(new Vector3(left, bottom, close) + pos, new Vector3(right, top, distant) + pos, this.debug);
         }
 
         protected override void RenderBody()
         {
             GL.PushMatrix();
-
-            GL.Scale(this.scale);
-
-            GL.Rotate(this.angles.X, Vector3.UnitY);
-            GL.Rotate(this.angles.Y, Vector3.UnitX);
-            GL.Rotate(this.angles.Z, Vector3.UnitZ);
 
             GL.Color4(color);
 
