1b1667c26373aa98a69eb6e227fd5eb77c1c60e7
majda107 <majda.trpkos@gmail.com>
2019-04-19 15:28:54 +0200

    Created physics elements

---
+++ Physics/APhysicElement.cs
@@ -0,0 +1,14 @@
+﻿using SharpEngine.Solids;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpEngine.Physics
+{
+    abstract class APhysicElement
+    {
+        abstract public void Update(ASolid solid);
+    }
+}
---
+++ Physics/RigidBody.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpEngine.Solids;
+
+namespace SharpEngine.Physics
+{
+    class RigidBody : APhysicElement
+    {
+        public bool Gravity { get; set; }
+
+        public float speed;
+        public float gravity;
+
+        public RigidBody()
+        {
+            this.Gravity = true;
+            this.speed = 0.05f;
+            this.gravity = 4f;
+        }
+        public override void Update(ASolid solid)
+        {
+            if(Gravity)
+            {
+                solid.pos.Y -= gravity * speed;
+                if (speed < gravity) speed += 0.05f;
+            }
+        }
+    }
+}
