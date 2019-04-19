39669d0e0533fb1450bd77c06fd8c0c3822488da
majda107 <majda.trpkos@gmail.com>
2019-04-19 15:28:31 +0200

    Created collision event args for collision events

---
+++ Gamelib/CollisionEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpEngine.Solids;
+
+namespace SharpEngine.Gamelib
+{
+    class CollisionEventArgs : EventArgs
+    {
+        public ASolid solid { get; private set; }
+        public CollisionEventArgs(ASolid solid)
+        {
+            this.solid = solid;
+        }
+    }
+}
