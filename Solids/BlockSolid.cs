ed821279cd040c95aae274645c54ea9460ae22fd
majda107 <majda.trpkos@gmail.com>
2019-04-19 15:31:04 +0200

    Fixed hitbox generation

--- Solids/BlockSolid.cs
+++ Solids/BlockSolid.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using SharpEngine.Gamelib;
 using OpenTK.Graphics.OpenGL;
 using OpenTK;
+using SharpEngine.ObjLoader;
 
 namespace SharpEngine.Solids
 {
@@ -20,18 +21,12 @@ namespace SharpEngine.Solids
             this.height = height;
             this.depth = depth;
 
-            this.hitbox = new Hitbox(new Vector3(pos.X + width / 2, pos.Y - height/2, pos.Z - depth / 2), new Vector3(pos.X - width / 2, pos.Y + height / 2, pos.Z + depth / 2));
+            this.hitbox = new Hitbox(new Vector3(pos.X + width / 2, pos.Y - height / 2, pos.Z - depth / 2), new Vector3(pos.X - width / 2, pos.Y + height / 2, pos.Z + depth / 2), this.debug);
         }
 
         protected override void RenderBody()
         {
             GL.PushMatrix();
-
-            GL.Scale(this.scale);
-
-            GL.Rotate(this.angles.X, Vector3.UnitY);
-            GL.Rotate(this.angles.Y, Vector3.UnitX);
-            GL.Rotate(this.angles.Z, Vector3.UnitZ);
 
             GL.Color4(color);
 
@@ -80,6 +75,11 @@ namespace SharpEngine.Solids
             GL.End();
 
             GL.PopMatrix();
+        }
+
+        protected override Hitbox UpdateHitbox(ref Face3[] faces)
+        {
+            return new Hitbox(new Vector3(pos.X + width / 2, pos.Y - height / 2, pos.Z - depth / 2), new Vector3(pos.X - width / 2, pos.Y + height / 2, pos.Z + depth / 2), this.debug);
         }
     }
 }
