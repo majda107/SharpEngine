be298c137e7b14d6db16cdbb1a41d7cd2ac20110
majda107 <majda.trpkos@gmail.com>
2019-04-19 21:46:20 +0200

    Added Render method

--- ObjLoader/Face3.cs
+++ ObjLoader/Face3.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTK;
+using OpenTK.Graphics.OpenGL;
 
 namespace SharpEngine.ObjLoader
 {
@@ -31,6 +32,25 @@ namespace SharpEngine.ObjLoader
             this.normals = normals;
 
             this.material = material;
+        }
+
+        public void Render()
+        {
+            if (this.material != null) this.material.Load(); // load material values
+            GL.BindTexture(TextureTarget.Texture2D, this.material.textureID); // bind material texture
+
+            GL.Begin(PrimitiveType.Triangles); // begin polygon drawing
+
+            for (int i = 0; i < 3; i++)
+            {
+                GL.Normal3(this.normals[i]);
+                GL.TexCoord2(this.textures[i]);
+                GL.Vertex3(this.vertices[i]);
+            }
+
+            GL.End();
+
+            GL.BindTexture(TextureTarget.Texture2D, 0);
         }
     }
 }
