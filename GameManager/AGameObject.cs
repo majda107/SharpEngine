21238290b7d0beeb3f0228eab70fea88773ede63
Marian Trpkos <majda.trpkos@gmail.com>
2019-04-30 23:46:04 +0200

    Added Pos property with automatical update

--- GameManager/AGameObject.cs
+++ GameManager/AGameObject.cs
@@ -1,14 +1,24 @@
-﻿using System;
+﻿using OpenTK;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OpenTK;
 
 namespace SharpEngine.GameManager
 {
     abstract class AGameObject
     {
-        public Vector3 pos;
+        private Vector3 _pos;
+
+        public Vector3 Pos { get => _pos; set {
+                if(_pos != value)
+                {
+                    _pos = value;
+                    this.Update();
+                }
+            } }
+
+        protected abstract void Update();
     }
 }
